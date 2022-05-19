f57264e9beda7efe2c9ecb2512cb97ea7aab68ca
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-05-19 10:19:04 +0600

    create search without api key

--- google_map_app/lib/src/module/home/page/view/home_view.dart
+++ google_map_app/lib/src/module/home/page/view/home_view.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
+import 'package:google_api_headers/google_api_headers.dart';
 import '../../data/location.dart' as locations;
 
 class HomeView extends StatefulWidget {
@@ -13,9 +17,12 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  final Map<String, Marker> _markers = {};
+  List<Marker> _markers = [];
+  final homeScaffoldKey = GlobalKey<ScaffoldState>();
+  late GoogleMapController googleMapController;
 
   Future<void> _onMapCreated(GoogleMapController controller) async {
+    googleMapController = controller;
     final googleOffices = await locations.getGoogleOffices();
     setState(() {
       _markers.clear();
@@ -28,7 +35,7 @@ class _HomeViewState extends State<HomeView> {
             snippet: office.address,
           ),
         );
-        _markers[office.name] = marker;
+        _markers.add(marker);
       }
     });
   }
@@ -36,31 +43,100 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: homeScaffoldKey,
       appBar: AppBar(
         title: const Text('Maps Sample App'),
+        actions: [
+          IconButton(
+            onPressed: _showSearch,
+            icon: const Icon(Icons.search_rounded),
+          ),
+        ],
       ),
       body: GoogleMap(
         mapType: MapType.normal,
         onMapCreated: _onMapCreated,
+        onTap: _onTabMap,
         initialCameraPosition: const CameraPosition(
           target: LatLng(42.86409393745507, 74.57551281899214),
           zoom: 12,
         ),
-        onTap: (val) async {
-          final address = await placemarkFromCoordinates(
-              val.latitude, val.longitude,
-              localeIdentifier: 'ru');
-          showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                title: const Text("My title"),
-                content: Text("${address[0]}"),
-              );
-            },
-          );
-        },
       ),
+    );
+  }
+
+  Future<void> _showSearch() async {
+    Prediction? p = await PlacesAutocomplete.show(
+      context: context,
+      apiKey: dotenv.get('googleNPlaceApiKey', fallback: 'Default fallback value'),
+      onError: onError,
+      mode: Mode.overlay,
+      language: 'en',
+      strictbounds: false,
+      types: [""],
+      decoration: InputDecoration(
+        hintText: 'Search',
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: Colors.white),
+        ),
+      ),
+      components: [
+        Component(Component.country, "pk"),
+        Component(Component.country, "usa"),
+      ],
+    );
+
+    displayPrediction(p!, homeScaffoldKey.currentState);
+  }
+
+  void onError(PlacesAutocompleteResponse response) {
+    print(response.errorMessage);
+    homeScaffoldKey.currentState!.showSnackBar(
+      SnackBar(
+        content: Text(response.errorMessage!),
+      ),
+    );
+  }
+
+  Future<void> displayPrediction(
+      Prediction p, ScaffoldState? currentState) async {
+    GoogleMapsPlaces places = GoogleMapsPlaces(
+        apiKey:
+            dotenv.get('googleNPlaceApiKey', fallback: 'Default fallback value'),
+        apiHeaders: await const GoogleApiHeaders().getHeaders());
+
+    PlacesDetailsResponse detail = await places.getDetailsByPlaceId(p.placeId!);
+
+    final lat = detail.result.geometry!.location.lat;
+    final lng = detail.result.geometry!.location.lng;
+
+    _markers.clear();
+    _markers.add(Marker(
+        markerId: const MarkerId("0"),
+        position: LatLng(lat, lng),
+        infoWindow: InfoWindow(title: detail.result.name)));
+
+    setState(() {});
+
+    googleMapController
+        .animateCamera(CameraUpdate.newLatLngZoom(LatLng(lat, lng), 14.0));
+  }
+
+  Future<void> _onTabMap(LatLng val) async {
+    final address = await placemarkFromCoordinates(
+      val.latitude,
+      val.longitude,
+      localeIdentifier: 'ru',
+    );
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("My title"),
+          content: Text("${address[0]}"),
+        );
+      },
     );
   }
 }

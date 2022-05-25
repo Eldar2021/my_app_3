c9ce582274ae4ae790c22066ee8c37144f67b869
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-05-25 13:57:34 +0600

    added json file and header content type

--- dart_frog_example/routes/hello.dart
+++ dart_frog_example/routes/hello.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'Hello Eldiyar');
+  final header = {'Content-type': 'application/json; charset=utf-8'};
+  return Response(body: fixture('user'), headers: header);
 }
+
+String fixture(String name) => File('json/$name.json').readAsStringSync();

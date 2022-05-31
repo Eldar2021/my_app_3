7806332e51bf57a1d9bd104eb41db4bbcc523466
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-05-31 15:45:18 +0600

    added elevation screen front

--- material_3_custom/lib/views/elevation_view.dart
+++ material_3_custom/lib/views/elevation_view.dart
@@ -1,16 +1,162 @@
 import 'package:flutter/material.dart';
 
+import 'buttons_view.dart';
+
+class ElevationInfo {
+  const ElevationInfo(
+    this.level,
+    this.elevation,
+    this.overlayPercent,
+    this.vals,
+  );
+  final int level;
+  final double elevation;
+  final int overlayPercent;
+  final double vals;
+}
+
+const List<ElevationInfo> elevations = <ElevationInfo>[
+  ElevationInfo(0, 0.0, 0, 0.1),
+  ElevationInfo(1, 1.0, 5, 0.3),
+  ElevationInfo(2, 3.0, 8, 0.5),
+  ElevationInfo(3, 6.0, 11, 0.7),
+  ElevationInfo(4, 8.0, 12, 0.9),
+  ElevationInfo(5, 12.0, 14, 1.0),
+];
+
 class ElevationScreen extends StatelessWidget {
-  const ElevationScreen({Key? key}) : super(key: key);
+  const ElevationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final scheme = Theme.of(context).colorScheme;
+    final tTheme = Theme.of(context).textTheme;
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('ElevationScreen'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Primery Container opasity',
+                style: tTheme.bodyLarge,
+              ),
+              Wrap(
+                children: elevations
+                    .map(
+                      (e) => ElevetionCard(
+                        color: scheme.primary,
+                        info: e,
+                        shadow: Theme.of(context).canvasColor,
+                        opasity: e.vals,
+                        index: e.level + 1,
+                      ),
+                    )
+                    .toList(),
+              ),
+              sizeH20,
+              Text(
+                'Primery Container opasity and Shadow',
+                style: tTheme.bodyLarge,
+              ),
+              Wrap(
+                children: elevations
+                    .map(
+                      (e) => ElevetionCard(
+                        color: scheme.primary,
+                        info: e,
+                        shadow: scheme.shadow,
+                        opasity: e.vals,
+                        index: e.level + 1,
+                        isShadow: true,
+                      ),
+                    )
+                    .toList(),
+              ),
+              sizeH20,
+              Text(
+                'Container only Shadow',
+                style: tTheme.bodyLarge,
+              ),
+              Wrap(
+                children: elevations
+                    .map(
+                      (e) => ElevetionCard(
+                        color: scheme.primary,
+                        info: e,
+                        shadow: scheme.shadow,
+                        opasity: e.vals,
+                        index: e.level + 1,
+                        isShadow: true,
+                        isColor: false,
+                      ),
+                    )
+                    .toList(),
+              ),
+              sizeH20,
+            ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('ElevationScreen'),
+    );
+  }
+}
+
+class ElevetionCard extends StatelessWidget {
+  const ElevetionCard({
+    super.key,
+    required this.color,
+    required this.shadow,
+    required this.info,
+    this.opasity = 0,
+    required this.index,
+    this.isShadow = false,
+    this.isColor = true,
+  });
+
+  final Color color;
+  final Color shadow;
+  final ElevationInfo info;
+  final double opasity;
+  final int index;
+  final bool isShadow;
+  final bool isColor;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final Color surface = Theme.of(context).colorScheme.surface;
+    final pr = opasity * 100;
+    return Padding(
+      padding: EdgeInsets.all(width / 100),
+      child: SizedBox(
+        width: width / 3.42,
+        height: width / 3.42,
+        child: Material(
+          type: MaterialType.card,
+          borderRadius: BorderRadius.circular(7),
+          color: surface,
+          surfaceTintColor: isColor ? color : null,
+          shadowColor: isShadow ? shadow : null,
+          elevation: info.elevation,
+          child: Padding(
+            padding: EdgeInsets.all(width / 100),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Level $index'),
+                Text('$index db'),
+                const Spacer(),
+                Align(
+                  alignment: Alignment.bottomRight,
+                  child: Text('$pr%'),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }

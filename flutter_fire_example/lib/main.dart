dafb7abc82e1b2d5b9b0ce432f954eac064252a3
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-06-08 12:06:55 +0600

    flutter fire example useMaterual3 change true

--- flutter_fire_example/lib/main.dart
+++ flutter_fire_example/lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );

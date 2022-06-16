91af537fcd41eb12ca83b58e4a909ca575a47bc9
eldiiar Almazbek <eldiiaralmazbekov@gmail.com>
2022-06-16 09:01:50 +0600

    added cli077

---
+++ cli077/android/app/src/main/kotlin/com/cli077org/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cli077org.cli077
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ cli077/lib/main_staging.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:cli077/app/app.dart';
+import 'package:cli077/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
